e1f74ed199ecd870f6c7e98ec414e67d66b6439c
MartynaKlebba <48858932+MartynaKlebba@users.noreply.github.com>
2019-04-27 21:20:47 +0200

    Update Main.java

--- lab3/zad7/src/Main.java
+++ lab3/zad7/src/Main.java
@@ -9,13 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         palindromy zdanie1 = new palindromy ("Atak kata");
-<<<<<<< HEAD
         palindromy zdanie2 = new palindromy ("Muzo raz daj jad za rozum");
         palindromy zdanie3 = new palindromy ("Wody zal dla zydow");
-=======
-        palindromy zdanie2 = new palindromy ("muzorazdajjadzarozum");
-        palindromy zdanie3 = new palindromy ("wodyzaldlazydow");
->>>>>>> e66d262e1cc6a25c51389b9c479d1d046b1acb07
+
 
         System.out.println(zdanie1.getPalindrom()+ " "+zdanie1.check());
         System.out.println(zdanie2.getPalindrom()+ " "+zdanie2.check());
