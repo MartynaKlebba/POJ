08378b9a380ec0d547f61d0ab032849078d56286
MartynaKlebba <48858932+MartynaKlebba@users.noreply.github.com>
2019-06-08 20:26:00 +0200

    Create UATicketInfo.java

---
+++ lab7/zad1/UATicketInfo.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Zadanie 1
+     */
+import java.time.LocalDateTime;
+
+public class UATicketInfo {
+    String from;
+    String to;
+    LocalDateTime dateTime;
+    double price;
+}
